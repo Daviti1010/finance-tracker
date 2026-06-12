5f90c1d3099329a7938437500639d7ef9895229c
Davit <davitmegrelishvili123@gmail.com>
2026-06-12 13:19:02 +0400

    Add login endpoint with JWT authentication

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import pool from "../db";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const router = express.Router();
 
@@ -45,5 +49,50 @@ router.post("/register", async (req, res) => {
         res.status(500).json({ success: false, message: "Server error" });
     }
 })
+
+
+router.post("/login", async (req, res) => {
+    const emailEnteredByUser = req.body.email;
+    const passwordEnteredByUser = req.body.password;
+
+    if (!emailEnteredByUser || !passwordEnteredByUser) {
+        return res.status(400).json({ success: false, message: "Incorrect email or password" })
+    }
+
+    try {
+        const result = await pool.query("SELECT * FROM users WHERE email = $1", [emailEnteredByUser]);
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({ success: false, message: "Invalid credentials" });
+        }
+
+        const user = result.rows[0];
+        const passwordSavedInDB = user.password_hash;
+
+        const userInfo = { id: user.id, name: user.name }
+
+        const isMatch = await bcrypt.compare(passwordEnteredByUser, passwordSavedInDB)
+
+        if (isMatch) {
+            
+            const accessToken = jwt.sign(userInfo, process.env.JWT_SECRET!, {
+                expiresIn: '7d'
+            })
+
+            res.status(200).json({ accessToken: accessToken })
+
+        } else {
+            return res.status(401).json({ success: false, message: "Invalid credentials" });
+        }
+
+
+    } catch (err) {
+        console.error("Login error:", err);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+})
+
+
+
 
 export default router;
