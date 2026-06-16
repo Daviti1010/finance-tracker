02f28e43ff4938c83437b7da2b6c3fc39bed4fb5
Davit <davitmegrelishvili123@gmail.com>
2026-06-16 16:22:45 +0400

    Add create transaction endpoint

--- server/routes/transactions.ts
+++ server/routes/transactions.ts
@@ -24,5 +24,34 @@ router.get("/", authMiddleware, async (req: any, res: any) => {
 })
 
 
+router.post("/", authMiddleware, async (req: any, res: any) => {
+    const {type, amount, category, description, date} = req.body;
+    const userId = req.user?.id;
+
+    if (!type || !amount || !category || !date) {
+        return res.status(400).json({success: false, message: "Missing a field" })
+    }
+
+    if (type !== "income" && type !== "expense" ) {
+        return res.status(400).json({success: false, message: "Invalid information" })
+    }
+
+    if (amount <= 0) {
+        return res.status(400).json({success: false, message: "Invalid number" })
+    }
+
+    try {
+        const result = await pool.query("INSERT INTO transactions (user_id, type, amount, category, description, date) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+            [userId, type, amount, category, description, date])
+
+        return res.status(201).json(result.rows[0])
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: "Server error" })
+    }
+})
+
+
 
 export default router
