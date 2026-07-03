cc5505716ab3347733a38a6e8e40906fca387762
Davit <davitmegrelishvili123@gmail.com>
2026-07-03 14:44:05 +0400

    Add income, expenses, and balance section

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,10 +1,36 @@
 import { Header } from "./HeaderPages/Header"
+import './Dashboard.css'
+
 
 export function Dashboard() {
     return (
         <>
             <Header />  
-            <div><h1>Dashboard</h1></div>
+
+            <body>
+                <div className="dashboard-page">
+                    <div className="income-expense-container">
+                        <div className="income-expense">
+                            <div className="income">
+                                <div id="income-text">Total income</div>
+                                <div id="income-number">$3,200.00</div>
+                            </div>
+                            <div className="expense">
+                                <div id="expense-text">Total expenses</div>
+                                <div id="expense-number">$1,840.00</div>
+                            </div>
+                            <div className="net-balance">
+                                <div id="net-balance-text">Net balance</div>
+                                <div id="net-balance-number">$1,360.00</div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="transaction-input"></div>
+                    <div className="all-transactions"></div>
+                </div>  
+            </body>
         </>
     )
 }
