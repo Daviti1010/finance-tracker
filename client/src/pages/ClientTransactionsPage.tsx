7cc2f7fcf1262d401acc14f388d6b03d76e40c67
Davit <davitmegrelishvili123@gmail.com>
2026-07-21 14:03:59 +0400

    feat(ClientFinancialSummary): add ClientFinancialSummary component

--- client/src/pages/ClientTransactionsPage.tsx
+++ client/src/pages/ClientTransactionsPage.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router";
 import { Header } from "./HeaderPages/Header"
-import { getClientTransactions, getClientStartingBalance } from "../api";
+import { ClientFinancialSummary } from "./ClientTransactionsPages/FinancialSummary/ClientFinancialSummary";
+import { getClientTransactions } from "../api";
 import type { Transaction } from "../types";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRotateLeft } from '@fortawesome/free-solid-svg-icons'
@@ -72,55 +73,6 @@ export function ClientTransactionsPage() {
         })
     }
 
-    // --------------------------------------------------------------------
-
-    const [netBalance, setNetBalance] = useState("");
-    const [balanceSavedInDB, setBalanceSavedInDB] = useState(false);
-
-    const totalIncome = allTransactions
-        .filter(t => t.type === "income")
-        .reduce((sum, t) => sum + Number(t.amount), 0)
-
-    const totalExpenses = allTransactions
-        .filter(t => t.type === "expense")
-        .reduce((sum, t) => sum + Number(t.amount), 0)
-
-    const formatMoney = (amount: number | string) => {
-        const formatter = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-        });
-
-        const numericAmount = amount ? parseFloat(amount.toString()) : 0;
-
-        return formatter.format(numericAmount);
-    };
-
-    useEffect(() => {
-        async function findStartingBalance() {
-            try {
-                const response = await getClientStartingBalance(clientIdNum);
-                const data = await response.json();
-
-                if (!response.ok) {
-                    console.log(data.message)
-                    return
-                }
-
-                if (data.startingBalance !== null && data.startingBalance !== undefined) {
-                    setNetBalance(String(data.startingBalance))
-                    setBalanceSavedInDB(true)
-                }
-
-            } catch (err) {
-                console.log(err)
-            }
-        }
-
-        findStartingBalance()
-    }, [clientIdNum])
-
-    // --------------------------------------------------------------------
 
     const fetchClientTransactions = useCallback(async (type?: string, category?: string) => {
         if (!clientId || isNaN(clientIdNum)) {
@@ -159,33 +111,9 @@ export function ClientTransactionsPage() {
     return (
         <>
         <Header />
+
+        <ClientFinancialSummary allTransactions={allTransactions} />
         
-        <div className="income-expense-container">
-                <div className="income-expense-balance">
-                    <div className="income-expense">
-                    <div className="income">
-                        <div id="income-text">Total income</div>
-                        <div id="income-number"><p>${totalIncome.toFixed(2)}</p></div>
-                    </div>
-                    <div className="expense">
-                        <div id="expense-text">Total expenses</div>
-                        <div id="expense-number"><p>${totalExpenses.toFixed(2)}</p></div>
-                    </div>
-                </div>
-                <div className="net-balance">
-                    <div id="net-balance-text">Net balance</div>
-    
-                    {balanceSavedInDB && (
-                        <div id="net-balance-number">
-                            <p id="net-balance-number-p">
-                                {formatMoney(Number(netBalance) + totalIncome - totalExpenses)}
-                            </p>
-                        </div>
-                    )}
-    
-                </div>
-            </div>
-        </div>
 
         <div className="all-transactions-container">
             <div className="upper-part">
